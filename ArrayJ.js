5a4d0a5443931ef952c79fd0a4284efbf6aa60b1
CaldRicardo <rickycalderon2006@gmail.com>
2025-02-24 10:40:06 -0400

    Anadir el archivo de JOSE

---
+++ ArrayJ.js
@@ -0,0 +1,14 @@
+let numeros = [1,2,3,4,5,6,7,8,9,10];
+let numerosDupli = numeros.map(function(numeros) {
+    return numeros * numeros;
+});
+
+console.log(numerosDupli);
+
+
+let numero = [1,2,3,4,5,6,7,8,9,10];
+let NumerosCuadrado = numero.filter(function(numero) {
+    return numero > 40;
+});
+
+console.log(numero);
